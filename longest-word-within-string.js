3c4ab583d6ec7833d20c36b3f298ac8c2361a2e3
aritzbo <71656434+aritzbo@users.noreply.github.com>
2020-09-25 18:23:24 +0200

    Update and rename find-the-longest-word-in-a-string.txt to longest-word-within-string.js

--- longest-word-within-string.js
+++ longest-word-within-string.js
@@ -1,5 +1,5 @@
-function findLongestWordLength(str) {
-  var words = str.split(' ');
+function longestWordLength(str) {
+  var words = str.split(" ");
   var maxLength = 0;
   for (var i = 0; i < words.length; i++) {
     if (words[i].length > maxLength) {
@@ -9,4 +9,4 @@ function findLongestWordLength(str) {
   return maxLength;
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+longestWordLength("The quick brown fox jumped over the lazy dog"); // Returns "jumped"
